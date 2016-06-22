a27e7b104496ec7c07f522206ff685d1e01339d7
Salbei <karsus3@web.de>
2016-06-22 12:23:26 +0200

    Add files via upload

---
+++ Suez.hpp
@@ -0,0 +1,20 @@
+class DefaultKeyPoint1
+{
+	name="Forest";
+	position[]={9306.58,12567.25};
+	type="VegetationFir";
+	
+	radiusA=1382.09;
+	radiusB=1077.02;
+	angle=0.000;
+};
+class DefaultKeyPoint2
+{
+	name="Village";
+	position[]={8761.64,13318.24};
+	type="NameVillage";
+	
+	radiusA=289.84;
+	radiusB=225.87;
+	angle=0.000;
+};
---
+++ cfgClutter.h
@@ -0,0 +1,13 @@
+class DefaultClutter;
+class clutter
+{
+	class Grass: DefaultClutter
+	{
+		model = "A3\Plants_F\Clutter\c_GrassGreen_GroupSoft.p3d";
+		affectedByWind = 0.0;
+		swLighting = 0;
+		scaleMin = 0.3;
+		scaleMax = 1.0;
+	};
+
+};
